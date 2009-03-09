19228fc1520fddce112a436f39de7115a9cc647e
Justin Azoff <justin@bouncybouncy.net>
2009-03-09 16:37:27 +0000

    sleep for at most 0.1 seconds

--- tcpsleep/client.py
+++ tcpsleep/client.py
@@ -1,7 +1,7 @@
 import socket
 def wakeup(port,host='127.0.0.1'):
     s=socket.socket()
-    s.setblocking(False)
+    s.settimeout(0.1)
     try :
         s.connect((host, port))
         s.shutdown(socket.SHUT_RDWR)

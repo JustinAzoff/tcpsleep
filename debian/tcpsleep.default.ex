edb1263d23e5e727cdbedba1bc1a7cd569f97fa0
Justin Azoff <justin@bouncybouncy.net>
2009-07-22 22:45:29 -0400

    first go at a debian package

---
+++ debian/tcpsleep.default.ex
@@ -0,0 +1,10 @@
+# Defaults for tcpsleep initscript
+# sourced by /etc/init.d/tcpsleep
+# installed at /etc/default/tcpsleep by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""

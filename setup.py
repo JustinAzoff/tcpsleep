8415fa825d8473cdf069b51e21533c20060178b6
Justin Azoff <justin@bouncybouncy.net>
2010-12-31 09:20:39 -0500

    bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name="tcpsleep",
-    version="1.0",
+    version="1.1",
     author="Justin Azoff",
     author_email="JAzoff@uamail.albany.edu",
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
